9c8b3029fec53925b97255c2e6e741615e31d596
holibhai <146805057+holibhai@users.noreply.github.com>
2025-03-07 15:47:10 +0530

    service class created

---
+++ src/main/java/com/crudoperation/jwt/service/UserDetailsImp.java
@@ -0,0 +1,21 @@
+package com.crudoperation.jwt.service;
+
+import com.crudoperation.jwt.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsImp implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(()->new UsernameNotFoundException("user not found"));
+    };
+}
