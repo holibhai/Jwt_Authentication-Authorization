132dedc257a9126e4444d3af7a5e6fd7f632beee
holibhai <146805057+holibhai@users.noreply.github.com>
2025-03-08 13:18:54 +0530

    authentication controller,authenticationService,DemoController classes created

--- src/main/java/com/crudoperation/jwt/controller/AuthenticationController.java
+++ src/main/java/com/crudoperation/jwt/controller/AuthenticationController.java
@@ -1,8 +1,29 @@
 package com.crudoperation.jwt.controller;
 
+import com.crudoperation.jwt.entity.AuthenticationResponse;
+import com.crudoperation.jwt.entity.UserAccount;
+import com.crudoperation.jwt.service.AuthenticationService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse>register(@RequestBody UserAccount request){
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse>login(@RequestBody UserAccount request){
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
 }
--- src/main/java/com/crudoperation/jwt/service/AuthenticationService.java
+++ src/main/java/com/crudoperation/jwt/service/AuthenticationService.java
@@ -3,8 +3,13 @@ package com.crudoperation.jwt.service;
 import com.crudoperation.jwt.entity.AuthenticationResponse;
 import com.crudoperation.jwt.entity.UserAccount;
 import com.crudoperation.jwt.repository.UserRepository;
+import org.springframework.expression.spel.ast.OpAnd;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AuthenticationService {
@@ -12,11 +17,13 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtSerivice jwtSerivice;
+    private final AuthenticationManager authenticationManager;
 
-    public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtSerivice jwtSerivice) {
+    public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtSerivice jwtSerivice, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtSerivice = jwtSerivice;
+        this.authenticationManager = authenticationManager;
     }
 
     public AuthenticationResponse register(UserAccount request ){
@@ -29,6 +36,19 @@ public class AuthenticationService {
 
         userAccount=userRepository.save(userAccount);
         String token = jwtSerivice.generateToken(userAccount);
+        return new AuthenticationResponse(token);
+    }
+
+    public AuthenticationResponse authenticate(UserAccount request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+            UserAccount user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token = jwtSerivice.generateToken(user);
+
         return new AuthenticationResponse(token);
     }
 }
