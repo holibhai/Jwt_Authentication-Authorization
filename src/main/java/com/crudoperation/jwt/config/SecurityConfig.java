565aebafdbc71cf55118153aca14a895e6c9e571
holibhai <146805057+holibhai@users.noreply.github.com>
2025-03-09 16:15:58 +0530

    minor change

--- src/main/java/com/crudoperation/jwt/config/SecurityConfig.java
+++ src/main/java/com/crudoperation/jwt/config/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
