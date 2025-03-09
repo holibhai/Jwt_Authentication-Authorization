b00b1b0be46722ad5d237d8a209bd32212bfec2a
holibhai <146805057+holibhai@users.noreply.github.com>
2025-03-09 14:25:47 +0530

    created demo contoller

--- src/main/java/com/crudoperation/jwt/controller/DemoController.java
+++ src/main/java/com/crudoperation/jwt/controller/DemoController.java
@@ -1,10 +1,13 @@
 package com.crudoperation.jwt.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5174")
+
 public class DemoController {
 
     @GetMapping("/demo")
